d9819a426b6c1e0e183e6025f68deb4f56433a02
user <user@DESKTOP-NU49PSL>
2021-12-29 10:45:49 +0900

    1st commit

---
+++ Project1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        int kg;
+        double gravity;
+         
+        kg = 49;
+        gravity= 0.2683;
+         
+        System.out.printf( "%d * %f = %f", kg, gravity, (kg * gravity) );
+    }
+}
